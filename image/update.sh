a89c43f49ac242f0b91a35ed1ab5e8630c876536
paulc <paulc@>
2020-12-17 14:50:10 +0000

    update

--- image/update.sh
+++ image/update.sh
@@ -7,14 +7,15 @@ set -o pipefail errexit nounset
 : ${IMAGE?ERROR: Must specify IMAGE}
 
 hcloud server create --location $LOCATION --type $TYPE --image $IMAGE --name img-update --user-data-from-file - <<'EOM'
-freebsd-update fetch --not-running-from-cron | cat
-freebsd-update install --not-running-from-cron || echo No updates available
-pkg update 
-pkg upgrade -y
-rm /var/hcloud/*
-rm /etc/ssh/*key*
-touch /firstboot
-shutdown -p now
+#!/bin/sh
+( freebsd-update fetch --not-running-from-cron | cat
+  freebsd-update install --not-running-from-cron || echo No updates available
+  pkg update 
+  pkg upgrade -y
+  rm /var/hcloud/*
+  rm /etc/ssh/*key*
+  touch /firstboot
+  shutdown -p now ) 2>&1 | tee /var/log/update-$(date +%Y%m%d-%H%M%S).log
 EOM
 
 hcloud server create-image --description "FreeBSD-12.2-base-cx11-$(date +%Y%m%d-%H%M%S)" --type snapshot img-update

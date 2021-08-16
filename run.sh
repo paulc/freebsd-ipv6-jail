57b8f202dbb550febbabd0af242908dbc2fa9aa8
paulc <paulc@>
2021-08-16 22:39:26 +0100

    Fix NAT64 host

--- run.sh
+++ run.sh
@@ -37,11 +37,7 @@ _log "env ASSUME_ALWAYS_YES=yes pkg bootstrap"
 _log "pkg update"
 
 # Install packages
-
-# Make sure we have latest version of python3
-_log "pkg remove -y python3 python37 || echo"
 _log "pkg install -y python3 git-lite rsync knot3"
-
 PY_VER=$(python3 -c 'import sys;print("py",sys.version_info.major,sys.version_info.minor,sep="")')
 
 _log "pkg install -y ${PY_VER}-pip"
